ea4bb14e5ea724d1a08fcafd24783731f5f10301
Арина Андреевна <Arina.Sheshukova@urfu.me>
2023-12-25 01:48:40 +0500

    Реализовала логику поиска опроса

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-//Шапка с навигационной панелью
+// Шапка с навигационной панелью
 
 export default function Header(): JSX.Element {
   return(
--- src/pages/all-survey-page/all-survey-page.tsx
+++ src/pages/all-survey-page/all-survey-page.tsx
@@ -1,10 +1,43 @@
 import Header from "../../components/header/header";
 import SurveyCardsList from "../../components/survey-cards-list/survey-cards-list";
+import { SurveyCard } from "../../types/survey-card";
 import AllSurveysPageProps from "./all-survey-page.props";
+import { ChangeEvent, useState, useEffect } from "react";
+
+// Фильтрация по поиску
+const filterBySearch = (searchValue: string, surveys: SurveyCard[]) => {
+    if (!searchValue) {
+      return surveys
+    } else {
+      return surveys.filter(({name}) => name.toLowerCase().includes(searchValue.toLocaleLowerCase()))
+    }
+};
 
 // Страница со всеми опросами
-
 export default function AllSurvey({surveys}: AllSurveysPageProps) :JSX.Element {
+  // Состояние поиска и функция для обновления
+  const [searchValue, setSearchValue] = useState('');
+  // Состояние списка с опросами, соответствующими поиску
+  const [relevantSurveys, setRelevantSurveys] = useState(surveys);
+
+  // Обработчик изменений поля поиска
+  const handleSearchChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const payload = evt.target.value
+    if (payload !== searchValue) {
+      setSearchValue(payload);
+    }
+  };
+
+  // Хук для изменения relevantSurveys при изменении значения поиска
+  useEffect(() => {
+    const Debounce = setTimeout(() => {
+      const filteredSurveys = filterBySearch(searchValue, surveys);
+      setRelevantSurveys(filteredSurveys)
+    }, 300);
+
+    return () => clearTimeout(Debounce);
+  }, [searchValue])
+
   return(
     <>
       <Header/>
@@ -17,9 +50,9 @@ export default function AllSurvey({surveys}: AllSurveysPageProps) :JSX.Element {
                 <div className="search">
                   <input
                     type="text"
-                    className=" search-field"
+                    className="search-field"
                     placeholder="Поиск по опросам"
-                    required={undefined}
+                    onChange={handleSearchChange}
                   />
                   <i
                     className="icon-search search-icon"
@@ -35,7 +68,7 @@ export default function AllSurvey({surveys}: AllSurveysPageProps) :JSX.Element {
                 </div>
               </div>
             </div>
-            <SurveyCardsList surveys={surveys}/>
+            <SurveyCardsList surveys={relevantSurveys}/>
           </main>
         </section>
       </div>
